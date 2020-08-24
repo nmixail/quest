7ba2080c4c23802791f4830b8fbb1b51847c9d0d
Michail Naiman <naiman.misha@gmail.com>
2020-08-24 23:32:31 +0300

    modify group_red page
    *Add new blocks
    *Add button click
    *Modify styles

---
+++ group_red/red.js
@@ -0,0 +1,64 @@
+"use strict";
+let button = document.querySelector("button");
+let input = document.querySelector("input");
+let count = 1;
+console.log(`div.class.stage_${count}`);
+button.onclick = function (event) {
+	let text = input.value.toLowerCase();
+	switch (text) {
+		case "одетый": {
+			if (count == 1) {
+				count++;
+				document.querySelector(`div.class.stage_${count}`).classList.add('b-show');
+				document.querySelector(`div.arrow.stage_${count}`).classList.add('b-show');
+			}
+			break;
+		}
+
+		case "в": {
+			if (count == 2) {
+				count++;
+				document.querySelector(`div.class.stage_${count}`).classList.add('b-show');
+				document.querySelector(`div.arrow.stage_${count}`).classList.add('b-show');
+			}
+			break;
+		}
+
+		case "форму,": {
+			if (count == 3) {
+				count++;
+				document.querySelector(`div.class.stage_${count}`).classList.add('b-show');
+				document.querySelector(`div.arrow.stage_${count}`).classList.add('b-show');
+			}
+			break;
+		}
+
+		case "спешишь начать": {
+			if (count == 4) {
+				count++;
+				document.querySelector(`div.class.stage_${count}`).classList.add('b-show');
+				document.querySelector(`div.arrow.stage_${count}`).classList.add('b-show');
+			}
+			break;
+		}
+
+		case "еще один": {
+			if (count == 5) {
+				count++;
+				document.querySelector(`div.class.stage_${count}`).classList.add('b-show');
+				document.querySelector(`div.arrow.stage_${count}`).classList.add('b-show');
+			}
+			break;
+		}
+
+		case "учебный год!": {
+			if (count == 6) {
+				count++;
+				document.querySelector(`div.class.stage_${count}`).classList.add('b-show');
+				document.querySelector(`div.arrow.stage_${count}`).classList.add('b-show');
+			}
+			break;
+		}
+
+	}
+};
